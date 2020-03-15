e49e196fb20ab632ddb6ea6b7514cbbc5be28705
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-03-15 04:32:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,6 @@ client.on('message', message => {
     if (message.content === 'ds.') {
     	message.reply('https://discord.gg/aprJuVg');
   	}
-client.on('ready', () => {
-    console.log('I am ready!');
 });
 
 client.on('message', message => {
