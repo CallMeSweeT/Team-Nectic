944a52aa427a22cdc2f63e1b08daa6f59daf7ac9
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-03-17 15:48:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!dis') {
+    if (message.content === '.dis') {
     	message.reply('https://discord.gg/aprJuVg');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === '!yt') {
+    if (message.content === '.yt') {
     	message.reply('https://www.youtube.com/channel/UCqehLUaBDWE2pU76SgW6eKw?view_as=subscriber');
   	}
 });
