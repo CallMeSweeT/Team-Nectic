62f4b6391a03f48596357685a5e6fb6fb1fdf719
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-03-18 20:53:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,8 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === '.code') {
-    	message.reply('.yt, .dis');
+    if (message.content === '.rc') {
+    	message.reply('Tags: Nectic, Nec');
   	}
 });
 
