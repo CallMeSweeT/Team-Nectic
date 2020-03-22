e6c4df88433120b99bd5cca6816381eaa39d5861
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-03-22 14:06:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,17 +17,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '.tags') {
-    	message.reply('Tags: Nectic, Nec');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '.info') {
-    	message.reply('Nectic Esport, is based on medie content. 70% of our team players are Eu, 21% Nae and 9% West. Nectic has existed in 1 week now.');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
