d2b7b3c0172d140dbf4bd60d733c5c5d6b019c2f
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-03-23 15:09:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === '.yt') {
-    	message.reply('https://www.youtube.com/channel/UCqehLUaBDWE2pU76SgW6eKw');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
