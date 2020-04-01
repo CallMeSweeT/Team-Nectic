7742cca399837863d49e9606f95141fb70610bbb
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-04-01 13:38:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '.scim') {
+    if (message.content === '.scrim') {
     	message.reply('https://discord.gg/WMYjAbv');
   	}
 });
